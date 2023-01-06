b7c7b5ce64cccd114a8d95f0ff6a303ba9b96d17
KodyAnderson1 <kody_anderson@yahoo.com>
2023-01-06 13:08:45 -0600

    Added faculty to backend

---
+++ studentsystem/src/main/java/com/kodyanderson/studentsystem/controller/FacultyController.java
@@ -0,0 +1,2 @@
+package com.kodyanderson.studentsystem.controller;public class FacultyController {
+}
---
+++ studentsystem/src/main/java/com/kodyanderson/studentsystem/service/FacultyServiceImpl.java
@@ -0,0 +1,2 @@
+package com.kodyanderson.studentsystem.service;public class FacultyServiceImpl {
+}
